c831cdbda65fccc1a26d26d6302e42ee7c95b933
affederaffe <68356204+affederaffe@users.noreply.github.com>
2021-05-30 13:39:50 +0200

    1.16.1

--- ImageCoverExpander/CoverExpander.cs
+++ ImageCoverExpander/CoverExpander.cs
@@ -34,10 +34,12 @@ namespace ImageCoverExpander
             _imageView.SetField("_gradient", true);
             _imageView.SetField("_color0", new Color(0.85f, 0.85f, 0.85f, 1f));
             _imageView.SetField("_color1", new Color(0.4f, 0.4f, 0.4f, 1f));
-            _imageView.transform.SetAsFirstSibling();
             RectTransform rectTransform = _imageView.rectTransform;
-            rectTransform.sizeDelta = new Vector2(75f, 75f);
-            rectTransform.localPosition = new Vector3(-40f, -57.5f, 0f);
+            rectTransform.sizeDelta = new Vector2(60f, 60f);
+            rectTransform.localPosition = new Vector3(-38.5f, -57f, 0f);
+            Transform transform = _imageView.transform;
+            transform.SetAsFirstSibling();
+            transform.localScale = new Vector3(1.15f, 1f, 1f);
         }
     }
 }
